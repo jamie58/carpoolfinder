2cb863a4fdad1b7cf0ec5e3099c1824be08cf84d
schott.brian <schott.brian@6998c81c-b746-11dd-8af8-77a15857c330>
2009-01-14 14:09:03 +0000

    added 'activity';soft-coded user_login in choose.html;added default=

--- maps.py
+++ maps.py
@@ -17,6 +17,7 @@ class Group(db.Model):
     people = db.StringProperty()
     contact =  db.StringProperty()
     moreinfo =  db.StringProperty()
+    activity =  db.StringProperty(default="live")
     user = db.UserProperty()
     
 class Pin(db.Model):
@@ -25,46 +26,6 @@ class Pin(db.Model):
     lng = db.FloatProperty()
     name = db.StringProperty()
 
-def make_new_map(place_id,self):
-      user = users.get_current_user()
-      logging.info("Make_new User %s " % user)
-      if user:    #send to add_place.html
-          logging.info("Make_new User if %s " % user)
-          place= Group(key_name=place_id)
-          place.place = place_id
-          place.user = user
-          place.people = ""
-          place.center_lat = float(-999)
-          place.center_lng = float(-999)
-          place.zoom = int(-999)
-          place.marker_lat = float(-999)
-          place.marker_lng = float(-999)
-          place.contact = ""
-          place.moreinfo = ""
-          place.put()
-          pin = Pin(key_name=place_id)
-          pin.name = place_id
-          #pin.lat = float(self.request.get('marker_lat'))
-          #pin.lng = float(self.request.get('marker_lng'))
-          pin.put()
-          #greeting = ("<a href=\"%s\">Sign out</a>." %
-          #        users.create_logout_url("/"))
-          #self.response.out.write("<html><body>%s</body></html>" % greeting)          
-          path = os.path.join(os.path.dirname(__file__), 'add_place.html')
-          template_values = dict(place=place.place, user=place.user) 
-          template_values = dict(place_id=place.key(), place=place.place, 
-                   people=place.people, center_lat=place.center_lat, 
-                   center_lng=place.center_lng, map_zoom=place.zoom,
-                   marker_lat=place.marker_lat, marker_lng=place.marker_lng, 
-                   contact=place.contact, moreinfo=place.moreinfo,
-                   user=place.user)
-          self.response.out.write(template.render(path, template_values))
-      else:     #send back to choose_place.html
-          logging.info("User else %s " % user)
-          greeting = ("<a href=\"%s\">Sign in or register</a>." %
-                  users.create_login_url("/"))
-          self.response.out.write("<html><body>%s</body></html>" % greeting)          
-                    
         
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -72,11 +33,25 @@ class MainPage(webapp.RequestHandler):
         if place_id is None:
             ####'choose_place' on next line does not seem to matter
             path = os.path.join(os.path.dirname(__file__), 'choose_place.html')
-            template_values = dict()
+            if users.get_current_user():
+              url = users.create_logout_url(self.request.uri)
+              url_linktext = 'Signout'
+              url_linktextmore = ' if you wish. You will be returned to this page after you signout.'
+            else:
+              url = users.create_login_url(self.request.uri)
+              url_linktext = 'Signin'
+              url_linktextmore = ' if you will be creating a map for your group. You will be returned to this page after you signin.'
+
+            template_values = {
+              'url': url,
+              'url_linktext': url_linktext,
+              'url_linktextmore': url_linktextmore,
+              }
             self.response.out.write(template.render(path, template_values))
         else:
-            ####go to map.html
+            ####determine if place exists already
             ####because choose_place.html has produced a desired place name
+            ####or because user has supplied a place name with ?...
             key = db.Key.from_path("Group", place_id)
             place = Group.get(key)
             if place : 
@@ -86,7 +61,7 @@ class MainPage(webapp.RequestHandler):
                          center_lng=place.center_lng, map_zoom=place.zoom,
                          marker_lat=place.marker_lat, marker_lng=place.marker_lng, 
                          contact=place.contact, moreinfo=place.moreinfo,
-                         user=place.user)
+                         activity=place.activity, user=place.user)
                 if user and user == place.user: #send user to add_place.html
                     logging.info("User if %s " % user)
                     logging.info("center_lat %s " % place.center_lat )
@@ -117,16 +92,16 @@ class OwnerMap(webapp.RequestHandler):
                 user = users.get_current_user()
                 logging.info("OwnerMap user %s " % user)
                 logging.info("OwnerMap place.user %s " % place.user)
-                if user and user == place.user: #send user to map.html
+                if user and user == place.user: #send user to mapowner.html
                     logging.info("User if %s " % user)
                     template_values = dict(place_id=place.key(), place=place.place, 
                          people=place.people, center_lat=place.center_lat, 
                          center_lng=place.center_lng, map_zoom=place.zoom,
                          marker_lat=place.marker_lat, marker_lng=place.marker_lng, 
                          contact=place.contact, moreinfo=place.moreinfo,
-                         user=place.user)
+                         activity=place.activity, user=place.user)
                     path = os.path.join(os.path.dirname(__file__), 'mapowner.html')
-                else:                           #send to map.html
+                else:                           #send to choose_place.html
                     template_values = dict()
                     path = os.path.join(os.path.dirname(__file__), 'choose_place')
                 self.response.out.write(template.render(path, template_values))
@@ -170,6 +145,7 @@ class AddPlace(webapp.RequestHandler):
         group.zoom = int(self.request.get('zoom'))
         group.people = self.request.get('people')
         group.contact = self.request.get('contact')
+        group.activity = self.request.get('activity')
         group.moreinfo = self.request.get('moreinfo')
         group.marker_lat = float(self.request.get('marker_lat'))
         group.marker_lng = float(self.request.get('marker_lng'))
@@ -194,6 +170,42 @@ application = webapp.WSGIApplication(
 
                                      
                                      
+def make_new_map(place_id,self):
+      user = users.get_current_user()
+      logging.info("Make_new User %s " % user)
+      if user:    #send to add_place.html
+          logging.info("Make_new User if %s " % user)
+          place= Group(key_name=place_id)
+          place.place = place_id
+          place.user = user
+          place.people = ""
+          place.center_lat = float(-999)
+          place.center_lng = float(-999)
+          place.zoom = int(-999)
+          place.marker_lat = float(-999)
+          place.marker_lng = float(-999)
+          place.contact = ""
+          place.activity = ""
+          place.moreinfo = ""
+          place.put()
+          pin = Pin(key_name=place_id)
+          pin.name = place_id
+          pin.put()
+          path = os.path.join(os.path.dirname(__file__), 'add_place.html')
+          template_values = dict(place=place.place, user=place.user) 
+          template_values = dict(place_id=place.key(), place=place.place, 
+                   people=place.people, center_lat=place.center_lat, 
+                   center_lng=place.center_lng, map_zoom=place.zoom,
+                   marker_lat=place.marker_lat, marker_lng=place.marker_lng, 
+                   contact=place.contact, moreinfo=place.moreinfo,
+                   activity=place.activity, user=place.user)
+          self.response.out.write(template.render(path, template_values))
+      else:     #send back to choose_place.html
+          logging.info("User else %s " % user)
+          greeting = ("<a href=\"%s\">Sign in or register</a>." %
+                  users.create_login_url("/"))
+          self.response.out.write("<html><body>%s</body></html>" % greeting)          
+                    
 def main():
     run_wsgi_app(application)
 
